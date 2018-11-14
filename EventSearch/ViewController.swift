cbd91b45b4641f433380b9fe163640ffd5b0ade4
Garrett Cone <garrettcone1@garretts-macbook-pro.local>
2018-11-13 18:43:54 -0600

    Initial Commit

---
+++ EventSearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EventSearch
+//
+//  Created by Garrett Cone on 11/13/18.
+//  Copyright © 2018 Garrett Cone. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
